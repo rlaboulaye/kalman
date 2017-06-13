81664a0b3c75f7d47e2832424be24acdc13c5d4a
Roland Laboulaye <rlaboulaye@gmail.com>
2017-06-13 16:18:47 -0600

    updated rrt node

--- rrt_node.py
+++ rrt_node.py
@@ -1,12 +1,20 @@
+from node import Node
 
-class RRTNode(object):
+class RRTNode(Node):
 
     current_id = 0
 
-    def __init__(self):
+    def __init__(self, pos):
+        super(RRTNode, self).__init__(pos)
+        self.parent = None
+        self.cost = 0
         self.id = RRTNode.current_id
         RRTNode.current_id += 1
-        self.parent = None
 
-    def add_parent(self, parent):
-        self.parent = parent
+    def set_parent(self, node):
+        self.parent = node
+        self.cost = node.cost + self.get_dist(node)
+
+    def get_distance(self, node):
+        return super(RRTNode, self).get_distance(node)
+
