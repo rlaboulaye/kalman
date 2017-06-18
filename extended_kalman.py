9e82c9665c985d4ccb19d7ab34df2492d17ef77a
Roland Laboulaye <rlaboulaye@gmail.com>
2017-06-17 20:44:13 -0600

    extended basic working

--- extended_kalman.py
+++ extended_kalman.py
@@ -2,10 +2,11 @@ from math import sin, cos, pi
 import numpy as np
 
 def step(mu_t, E_t, E_x, H, E_z, z_tp1, delta_t):
+    mu_prediction = apply_f(mu_t, delta_t)
     F = get_jacobian(mu_t, delta_t)
     identity_dim = F.shape[0]
     K_tp1 = np.dot(np.dot(np.add(np.dot(np.dot(F, E_t), F.T), E_x), H.T), np.linalg.inv(np.add(np.dot(np.dot(H, np.add(np.dot(np.dot(F, E_t), F.T), E_x)), H.T), E_z)))
-    mu_tp1 = np.add(apply_f(mu_t, delta_t), np.dot(K_tp1, np.subtract(z_tp1, np.dot(H, np.dot(F, mu_t)))))
+    mu_tp1 = np.add(mu_prediction, np.dot(K_tp1, np.subtract(z_tp1, np.dot(H, mu_prediction))))
     E_tp1 = np.dot(np.subtract(np.identity(identity_dim), np.dot(K_tp1, H)), np.add(np.dot(F, np.dot(E_t, F.T)), E_x))
     return (mu_tp1, E_tp1)
 
@@ -37,7 +38,7 @@ def apply_f(mu_t, delta_t):
     vec[3,0] = vl + delta_t * al
     vec[4,0] = ar
     vec[5,0] = al
-    vec[6,0] = (theta + (w * delta_t)) % (2 * pi)
+    vec[6,0] = (theta + (w * delta_t)) #% (2 * pi)
     return vec
 
 def get_jacobian(mu_t, delta_t):
@@ -115,25 +116,27 @@ def get_jacobian(mu_t, delta_t):
     return jac
 
 # px, py, vr, vl, ar, al
-E_x = np.array([[1, 0, 0, 0, 0, 0, 0],[1, 0, 0, 0, 0, 0, 0],[0, 0, 5, 0, 0, 0, 0],[0, 0, 0, 5, 0, 0, 0],[0, 0, 0, 0, 10, 0, 0],[0, 0, 0, 0, 0, 10, 0],[0, 0, 0, 0, 0, 0, .3]])
+E_x = np.array([[1, 0, 0, 0, 0, 0, 0],[1, 0, 0, 0, 0, 0, 0],[0, 0, 4, 0, 0, 0, 0],[0, 0, 0, 4, 0, 0, 0],[0, 0, 0, 0, 10, 0, 0],[0, 0, 0, 0, 0, 10, 0],[0, 0, 0, 0, 0, 0, .4]])
 H = np.array([[1, 0, 0, 0, 0, 0, 0],[0, 1, 0, 0, 0, 0, 0],[0, 0, 1, 0, 0, 0, 0],[0, 0, 0, 1, 0, 0, 0],[0, 0, 0, 0, 0, 0, 1]])
-E_z = np.array([[5, 0, 0, 0, 0],[0, 5, 0, 0, 0],[0, 0, 5, 0, 0],[0, 0, 0, 5, 0],[0, 0, 0, 0, .1]])
+E_z = np.array([[4, 0, 0, 0, 0],[0, 4, 0, 0, 0],[0, 0, 5, 0, 0],[0, 0, 0, 5, 0],[0, 0, 0, 0, .05]])
 
 mu_t = np.zeros((7,1))
 E_t = np.zeros((7,7))
 
 actual = np.copy(mu_t)
-actual[2] = 2
-actual[3] = 1
+actual[2] = 3
+actual[3] = 2.9
 delta_t = 1
 
-for i in range(10):
+for i in range(5):
     actual = apply_f(actual, delta_t)
     z_tp1 = np.append(actual[:4], actual[6]).reshape(-1,1)
     print('i: ', i)
     print(actual)
+    print()
     mu_t, E_t = step(mu_t, E_t, E_x, H, E_z, z_tp1, delta_t)
     print(mu_t)
+    print()
     print()
 
 
